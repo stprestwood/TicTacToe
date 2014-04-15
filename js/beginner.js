68d5716ab76b41eab868b23d11d2f0ec94ac204e
javacrash1 <javacrash1@B-8.local>
2014-04-15 13:41:26 -0700

    The begginer exercise changes were made

--- js/beginner.js
+++ js/beginner.js
@@ -1,5 +1,7 @@
 //Define a variable here called currentTurn and make it equal to the STRING "X"
 
+var currentTurn = "X";
+
 var TTT = {
 	changeElement: function(piece) {
 		if (currentTurn == "X") {
@@ -8,12 +10,14 @@ var TTT = {
 			this.checkWin();
 			
 			//Make currentTurn equal to "O"
+			currentTurn = "O";
 		} else {
 			$("#" + piece).html("O");
 			
 			this.checkWin();
 			
 			//Make currentTurn equal to "X"
+			currentTurn = "X";
 		}
 	},
 	
@@ -31,26 +35,38 @@ var TTT = {
 		
 		//Write a function here called sayWinner() that will alert the current turn letter plus the word "Wins". An example could be X Wins! Or O Wins!
 		
+		function sayWinner() {
+			alert(currentTurn + "Wins");
+		}
+
 		if (rowA[0] === rowA[1] && rowA[2] === rowA[0] && rowA[0] !== "") {
 			//Call the function sayWinner here
+			sayWinner();
 		} else if (rowB[0] === rowB[1] && rowB[2] === rowB[0] && rowB[0] !== "") {
 			//Call the function sayWinner here
+			sayWinner();
 		} else if (rowC[0] === rowC[1] && rowC[2] === rowC[0] && rowC[0] !== "") {
 			//Call the function sayWinner here
+			sayWinner();
 		}
 		
 		if (colA[0] === colA[1] && colA[2] === colA[0] && colA[0] !== "") {
 			//Call the function sayWinner here
+			sayWinner();
 		} else if (colB[0] === colB[1] && colB[2] === colB[0] && colB[0] !== "") {
 			//Call the function sayWinner here
+			sayWinner();
 		} else if (colC[0] === colC[1] && colC[2] === colC[0] && colC[0] !== "") {
 			//Call the function sayWinner here
+			sayWinner();
 		}
 		
 		if (diagOne[0] === diagOne[1] && diagOne[2] === diagOne[0] && diagOne[0] !== "") {
 			//Call the function sayWinner here
+			sayWinner();
 		} else if (diagTwo[0] === diagTwo[1] && diagTwo[2] === diagTwo[0] && diagTwo[0] !== "") {
 			//Call the function sayWinner here
+			sayWinner();
 		}
 	}
 };
@@ -58,9 +74,11 @@ var TTT = {
 $(".game-board td").on("click", function() {
 	if ($(this).html() !== "") {
 		//Write an alert here that says "This spot has been taken...Sorry!"
+		alert("This spot has already been taken.")
 		return false;
 	} else {
 		var gamePiece = $(this).attr("id");
 		//Call the function TTT.changeElement passing in the above variable (gamePiece) as a parameter
+		TTT.changeElement(gamePiece);
 	}
 });
